df41043b751f248bf731c957a699a754dec6ccd7
Li Liangcan <lliangcan@gmail.com>
2026-09-15 12:16:06 +0800

    feat: version2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello SRE Commander! Version 1")
+		fmt.Fprintf(w, "Hello SRE Commander! Version 2 - GitOps Activated!")
 	})
 	fmt.Println("Server starting on port 8080...")
 	http.ListenAndServe(":8080", nil)
