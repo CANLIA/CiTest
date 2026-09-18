d70e0c0393d7ebc615be092b31855229849351bb
Li Liangcan <lliangcan@gmail.com>
2026-09-18 10:56:05 +0800

    feat: fix the deployment yaml typo

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello SRE Commander! Version 2 - GitOps Activated!")
+		fmt.Fprintf(w, "Hello SRE Commander! Version 333 - GitOps Activated!")
 	})
 	fmt.Println("Server starting on port 8080...")
 	http.ListenAndServe(":8080", nil)
